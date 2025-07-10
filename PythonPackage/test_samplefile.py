ea05e2581ca2f8fa6b98eed8aeef5868d9979495
ArtenGangwar <arten.discovery@gmail.com>
2025-07-10 11:07:19 +0530

    test

---
+++ PythonPackage/test_samplefile.py
@@ -0,0 +1,36 @@
+def test_sample():
+    print("Hello Guys")
+
+
+
+def test_login_id():
+    a = 5
+    b = 5
+    assert a == b
+
+def test_login_id():
+    a = 5
+    b = 5
+    assert a >= b
+
+
+def test2_login_id():
+    a = 5
+    b = 6
+    assert a != b
+
+
+def test_logoff_id():
+    print("Element is visible.")
+    assert "test case fail"
+
+def test_click():
+    print("hello world!")
+
+
+def test_print():
+    a=3
+    b=5
+    assert a*b==b*a
+
+
---
+++ base_pages/test_usertype.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.sanity
+def test_teacher():
+    print("teacher should be 18 years old")
